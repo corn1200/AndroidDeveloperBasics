3c789aaaab094568a705bbb4b7e2fdfce6e17b26
corn1200 <corn120033>
2021-05-17 11:06:05 +0900

    feat: Android 기초 01.1 작업 6 완료

--- app/src/main/java/com/example/androiddeveloperbasics/MainActivity.java
+++ app/src/main/java/com/example/androiddeveloperbasics/MainActivity.java
@@ -13,9 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d(LOG_TAG, "Hello World");
-//        Log: Logcat 창에 로그 메시지를 보내기위한 클래스
-//        d: 디버그 수준, e: 에러 수준, w: 경고 수준, i: 정보 수준
-//        첫 번째 인수는 Logcat 창에서 메시지를 필터링하는 데 사용하는 태그
+        Log.i(LOG_TAG, "Hello World");
+
     }
 }
