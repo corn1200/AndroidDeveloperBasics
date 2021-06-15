d25c7b51cb8d6ace0954fd2fa68fa512af50bb7e
corn1200 <corn120033@gmail.com>
2021-06-15 14:59:27 +0900

    feat: Android 기초 01.2 작업 6 완료

--- app/src/main/java/com/example/androiddeveloperbasics/MainActivity.java
+++ app/src/main/java/com/example/androiddeveloperbasics/MainActivity.java
@@ -4,16 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private int mCount = 0;
+    private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mShowCount = findViewById(R.id.show_count);
 
         Log.i(LOG_TAG, "Hello World");
+    }
 
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
+
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null) {
+            mShowCount.setText(Integer.toString(mCount));
+        }
     }
 }
