398bc8a376c1e30aae85f0f0ddb299a341b72f3b
corn1200 <corn120033@gmail.com>
2021-06-18 19:52:55 +0900

    feat: Android 기초 01.4 작업 2 / 2.1 기본 활동 아키텍처 탐색 작업(임시)

--- app/src/main/java/com/example/androiddeveloperbasics/MainActivity.java
+++ app/src/main/java/com/example/androiddeveloperbasics/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.androiddeveloperbasics;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,28 +11,38 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
-    private int mCount = 0;
-    private TextView mShowCount;
+    private View decorView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        mShowCount = findViewById(R.id.show_count);
 
         Log.i(LOG_TAG, "Hello World");
+
+        decorView = getWindow().getDecorView();
+        decorView.setOnSystemUiVisibilityChangeListener
+                (new View.OnSystemUiVisibilityChangeListener() {
+            @Override
+            public void onSystemUiVisibilityChange(int visibility) {
+                if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+
+                } else {
+//                    풀 스크린 시 액션바 숨기기
+                    ActionBar actionBar = getSupportActionBar();
+                    actionBar.hide();
+                }
+            }
+        });
     }
 
-    public void showToast(View view) {
-        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
-        toast.show();
-
-    }
-
-    public void countUp(View view) {
-        mCount++;
-        if (mShowCount != null) {
-            mShowCount.setText(Integer.toString(mCount));
+//    액티비티가 포커스 되었을 경우 실행
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+//            풀 스크린
+            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
         }
     }
 }
