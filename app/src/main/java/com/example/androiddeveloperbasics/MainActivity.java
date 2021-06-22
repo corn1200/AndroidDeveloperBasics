c2540d6d5c8759afb09f88a1a891ba839ef830a4
corn1200 <corn120033@gmail.com>
2021-06-22 14:11:22 +0900

    feat: Android 기초 01.4 작업 4 완료

--- app/src/main/java/com/example/androiddeveloperbasics/MainActivity.java
+++ app/src/main/java/com/example/androiddeveloperbasics/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.androiddeveloperbasics;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.i(LOG_TAG, "Hello World");
+        Toolbar myToolBar = findViewById(R.id.toolbar);
+        setSupportActionBar(myToolBar);
 
         decorView = getWindow().getDecorView();
         decorView.setOnSystemUiVisibilityChangeListener
@@ -36,13 +40,39 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-//    액티비티가 포커스 되었을 경우 실행
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        if (hasFocus) {
-//            풀 스크린
-            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
-            decorView.setSystemUiVisibility(uiOptions);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Toast.makeText(this.getApplicationContext(), "action_settings", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_favorite:
+                Toast.makeText(this.getApplicationContext(), "action_favorite", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
         }
     }
+
+//    액티비티가 포커스 되었을 경우 실행
+//    public void onWindowFocusChanged(boolean hasFocus) {
+//        super.onWindowFocusChanged(hasFocus);
+//        if (hasFocus) {
+////            풀 스크린
+//            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+//            decorView.setSystemUiVisibility(uiOptions);
+//        }
+//    }
 }
