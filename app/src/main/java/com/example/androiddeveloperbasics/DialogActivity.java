9e4edb9695cdda832130d1f38934e7a8cf8dc3d0
corn1200 <corn120033@gmail.com>
2021-08-01 22:35:15 +0900

    feat: Android 기초 04.3 작업 5 완료

--- app/src/main/java/com/example/androiddeveloperbasics/DialogActivity.java
+++ app/src/main/java/com/example/androiddeveloperbasics/DialogActivity.java
@@ -1,12 +1,15 @@
 package com.example.androiddeveloperbasics;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.DialogFragment;
 
 public class DialogActivity extends AppCompatActivity {
 
@@ -14,6 +17,22 @@ public class DialogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dialog);
+
+        Toolbar myToolBar = findViewById(R.id.toolbar);
+        setSupportActionBar(myToolBar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return new MainToolbar().onSelectItems(item, this);
     }
 
     public void onCLickShowAlert(View view) {
@@ -25,13 +44,32 @@ public class DialogActivity extends AppCompatActivity {
 
 //        Add the dialog buttons.
         myAlertBuilder.setPositiveButton("OK", (dialog, which) -> {
-            Toast.makeText(getApplicationContext(), "Pressed OK", Toast.LENGTH_SHORT).show();
+//            User clicked OK button.
+            Toast.makeText(getApplicationContext(), "Pressed OK",
+                    Toast.LENGTH_SHORT).show();
         });
         myAlertBuilder.setNegativeButton("Cancel", (dialog, which) -> {
-            // User cancelled the dialog.
+//            User cancelled the dialog.
             Toast.makeText(getApplicationContext(), "Pressed Cancel",
                     Toast.LENGTH_SHORT).show();
         });
+
+//        Create and show the AlertDialog.
         myAlertBuilder.show();
+    }
+
+    public void showDatePicker(View view) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public void processDatePickerResult(int year, int month, int day) {
+        String month_string = Integer.toString(month+1);
+        String day_string = Integer.toString(day);
+        String year_string = Integer.toString(year);
+        String dateMessage = (month_string +
+                "/" + day_string + "/" + year_string);
+        Toast.makeText(this, "Date: " + dateMessage,
+                Toast.LENGTH_SHORT).show();
     }
 }
