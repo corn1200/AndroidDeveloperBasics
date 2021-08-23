ee9bb9888e5b18aa2c1d007c332b40b1f32c8418
corn1200 <corn120033@gmail.com>
2021-08-23 17:51:54 +0900

    feat: Android 기초 04.5 작업 2

--- app/src/main/java/com/example/androiddeveloperbasics/RecyclerActivity.java
+++ app/src/main/java/com/example/androiddeveloperbasics/RecyclerActivity.java
@@ -2,6 +2,8 @@ package com.example.androiddeveloperbasics;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,6 +13,8 @@ import java.util.LinkedList;
 
 public class RecyclerActivity extends AppCompatActivity {
     private final LinkedList<String> mWordList = new LinkedList<>();
+    private RecyclerView mRecyclerView;
+    private WordListAdapter mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +28,15 @@ public class RecyclerActivity extends AppCompatActivity {
         for (int i = 0; i < 20; i++) {
             mWordList.addLast("Word " + i);
         }
+
+//        Get a handle to the RecyclerView.
+        mRecyclerView = findViewById(R.id.recyclerview);
+//        Create an adapter and supply the date to be displayed.
+        mAdapter = new WordListAdapter(this, mWordList);
+//        Connect the adapter with the RecyclerView.
+        mRecyclerView.setAdapter(mAdapter);
+//        Give the RecyclerView a default layout manager.
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     @Override
