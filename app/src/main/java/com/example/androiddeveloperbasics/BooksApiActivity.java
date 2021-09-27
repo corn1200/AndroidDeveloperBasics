ca8e9d5a743f766ae60ea5b6be5727fa3cabec74
corn1200 <corn120033>
2021-09-27 15:32:46 +0900

    feat: Android 기초 07.2 작업 4

--- app/src/main/java/com/example/androiddeveloperbasics/BooksApiActivity.java
+++ app/src/main/java/com/example/androiddeveloperbasics/BooksApiActivity.java
@@ -18,6 +18,10 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.loader.app.LoaderManager;
 import androidx.loader.content.Loader;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class BooksApiActivity extends AppCompatActivity
         implements LoaderManager.LoaderCallbacks<String> {
     private EditText mBookInput;
@@ -37,6 +41,10 @@ public class BooksApiActivity extends AppCompatActivity
         mBookInput = findViewById(R.id.bookInput);
         mTitleText = findViewById(R.id.titleText);
         mAuthorText = findViewById(R.id.authorText);
+
+        if (getSupportLoaderManager().getLoader(0) != null) {
+            getSupportLoaderManager().initLoader(0, null, this);
+        }
     }
 
     @Override
@@ -93,12 +101,58 @@ public class BooksApiActivity extends AppCompatActivity
     @NonNull
     @Override
     public Loader<String> onCreateLoader(int id, @Nullable Bundle args) {
-        return null;
+        String queryString = "";
+
+        if (args != null) {
+            queryString = args.getString("queryString");
+        }
+
+        return new BookLoader(this, queryString);
     }
 
     @Override
     public void onLoadFinished(@NonNull Loader<String> loader, String data) {
+        try {
+            JSONObject jsonObject = new JSONObject(data);
+            JSONArray itemsArray = jsonObject.getJSONArray("items");
 
+            int i = 0;
+            String title = null;
+            String authors = null;
+
+            while (i < itemsArray.length() &&
+                    (authors == null && title == null)) {
+                // Get the current item information.
+                JSONObject book = itemsArray.getJSONObject(i);
+                JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+
+                // Try to get the author and title from the current item,
+                // catch if either field is empty and move on.
+                try {
+                    title = volumeInfo.getString("title");
+                    authors = volumeInfo.getString("authors");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                // Move to the next item.
+                i++;
+            }
+
+            if (title != null && authors != null) {
+                mTitleText.setText(title);
+                mAuthorText.setText(authors);
+            } else {
+                mTitleText.setText(R.string.no_results);
+                mAuthorText.setText("");
+            }
+        } catch (JSONException e) {
+            // If onPostExecute does not receive a proper JSON string,
+            // update the UI to show failed results.
+            mTitleText.setText(R.string.no_results);
+            mAuthorText.setText("");
+            e.printStackTrace();
+        }
     }
 
     @Override
