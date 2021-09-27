0f5631df97d8b2dfb78bff1184d0fb6ab8749982
corn1200 <corn120033>
2021-09-27 14:57:22 +0900

    feat: Android 기초 07.2 작업 3

--- app/src/main/java/com/example/androiddeveloperbasics/BooksApiActivity.java
+++ app/src/main/java/com/example/androiddeveloperbasics/BooksApiActivity.java
@@ -1,16 +1,25 @@
 package com.example.androiddeveloperbasics;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class BooksApiActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
+
+public class BooksApiActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<String> {
     private EditText mBookInput;
     private TextView mTitleText;
     private TextView mAuthorText;
@@ -45,6 +54,55 @@ public class BooksApiActivity extends AppCompatActivity {
     public void searchBooks(View view) {
 //        Get the search string from the input field.
         String queryString = mBookInput.getText().toString();
-        new FetchBook(mTitleText, mAuthorText).execute(queryString);
+
+        InputMethodManager inputManager = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        if (inputManager != null) {
+            inputManager.hideSoftInputFromWindow(view.getWindowToken(),
+                    InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = null;
+        if (connMgr != null) {
+            networkInfo = connMgr.getActiveNetworkInfo();
+        }
+
+        if (networkInfo != null && networkInfo.isConnected()
+                && queryString.length() != 0) {
+            new FetchBook(mTitleText, mAuthorText).execute(queryString);
+            mAuthorText.setText("");
+            mTitleText.setText(R.string.loading);
+        } else {
+            if (queryString.length() == 0) {
+                mAuthorText.setText("");
+                mTitleText.setText(R.string.no_search_term);
+            } else {
+                mAuthorText.setText("");
+                mTitleText.setText(R.string.no_network);
+            }
+        }
+
+        Bundle queryBundle = new Bundle();
+        queryBundle.putString("queryString", queryString);
+        getSupportLoaderManager().restartLoader(0, queryBundle, this);
+    }
+
+    @NonNull
+    @Override
+    public Loader<String> onCreateLoader(int id, @Nullable Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<String> loader, String data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<String> loader) {
+
     }
 }
