71fb3a2fbf91e1072eab24c40ba1b58eff55f621
corn1200 <corn120033@gmail.com>
2021-09-28 09:55:27 +0900

    feat: Android 기초 07.3 작업 2

--- app/src/main/java/com/example/androiddeveloperbasics/BroadcastReceiverActivity.java
+++ app/src/main/java/com/example/androiddeveloperbasics/BroadcastReceiverActivity.java
@@ -2,18 +2,26 @@ package com.example.androiddeveloperbasics;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.View;
 
 public class BroadcastReceiverActivity extends AppCompatActivity {
     private CustomReceiver mReceiver = new CustomReceiver();
+    private static final String ACTION_CUSTOM_BROADCAST =
+            BuildConfig.APPLICATION_ID + ".ACTION_CUSTOM_BROADCAST";
 
     @Override
     protected void onDestroy() {
         // Unregister the receiver
         this.unregisterReceiver(mReceiver);
+
+        LocalBroadcastManager.getInstance(this)
+                .unregisterReceiver(mReceiver);
+
         super.onDestroy();
     }
 
@@ -34,5 +42,13 @@ public class BroadcastReceiverActivity extends AppCompatActivity {
 
         // Register the receiver using the activity context.
         this.registerReceiver(mReceiver, filter);
+
+        LocalBroadcastManager.getInstance(this)
+                .registerReceiver(mReceiver, new IntentFilter(ACTION_CUSTOM_BROADCAST));
+    }
+
+    public void sendCustomBroadcast(View view) {
+        Intent customBroadcastIntent = new Intent(ACTION_CUSTOM_BROADCAST);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(customBroadcastIntent);
     }
 }
