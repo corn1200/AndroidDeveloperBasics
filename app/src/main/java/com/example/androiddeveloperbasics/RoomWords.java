7e679f9bff1cc67f39cb33ced4671556c5e38ddf
corn1200 <corn120033@gmail.com>
2021-11-08 15:31:09 +0900

    feat: Android 기초 10.1 파트 A 작업 1 / 종속성 오류 수정 & 툴바 추가

--- app/src/main/java/com/example/androiddeveloperbasics/RoomWords.java
+++ app/src/main/java/com/example/androiddeveloperbasics/RoomWords.java
@@ -1,8 +1,11 @@
 package com.example.androiddeveloperbasics;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class RoomWords extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class RoomWords extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room_words);
+
+        // 액션바 세팅
+        Toolbar myToolBar = findViewById(R.id.toolbar);
+        setSupportActionBar(myToolBar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return new MainToolbar().onSelectItems(item, this);
     }
 }
